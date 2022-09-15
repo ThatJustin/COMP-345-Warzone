4b65fb8029381a9aa343f69aec65a29d824c1100
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2022-09-14 20:51:07 -0400

    removed MapDriver.cpp from the CMakeLists txt file executable instructions

--- Map.cpp
+++ Map.cpp
@@ -1,1 +1,14 @@
 #include "Map.h";
+#include <iostream>
+#include <fstream>
+using std::cout;
+
+ifstream fin("/MapFiles/World.map");
+
+if(fin.fail( )) {
+    cout << "Error - Failed to open " << endl;
+    exit( -1 );
+}
+
+cout << "Success - Opened " << endl;
+
