23f6fa8fe511900af52750f4646f949cfa8d17ab
Alex De Luca <deluca@Alexs-MacBook-Air.local>
2022-09-18 00:33:32 -0400

    move function made

--- OrdersList.cpp
+++ OrdersList.cpp
@@ -11,9 +11,30 @@ OrdersList::OrdersList(const OrdersList& ol){
 
 void OrdersList::move(int to_move, int move_to) {
     Orders* temp;
-    temp = &list[move_to];
-    list[move_to]=list[to_move];
-    list.push_back(*temp);
+    if(to_move<move_to){
+        temp = &list[move_to];
+        list[move_to]=list[to_move];
+        list[to_move]=*temp;
+        list.push_back(*temp);
+        list.erase(list.begin()+to_move);
+        list.push_back(list[move_to]);
+        list.erase(list.begin()+move_to);
+        temp = &list[move_to];
+        list[move_to]=list[move_to-1];
+        list[move_to-1]=*temp;
+    }
+    if(to_move>move_to){
+        temp = &list[to_move];
+        list[to_move]=list[move_to];
+        list[move_to]=*temp;
+        list.push_back(*temp);
+        list.erase(list.begin()+move_to);
+        list.push_back(list[to_move]);
+        list.erase(list.begin()+to_move);
+        temp = &list[to_move];
+        list[to_move]=list[to_move-1];
+        list[to_move-1]=*temp;
+    }
 }
 
 void OrdersList::remove(){
