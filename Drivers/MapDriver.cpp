5ebc79d7668b8ce6304bfd25bd6228a58c8bc243
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2022-09-19 02:55:14 -0400

    just pushing what I have right now, not done yet; MapLoader loadMap and MapDriver testLoadMaps functions/methods still need to be completed/implemented

--- Drivers/MapDriver.cpp
+++ Drivers/MapDriver.cpp
@@ -4,65 +4,24 @@
 #include <vector>
 #include "sources/Map//Map.h"
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::ifstream;
-using std::vector;
-using std::cerr;
-using std::istringstream;
-
 void testLoadMaps() {
-    ifstream mapFile("./Map Files/World.map");
-    vector<string> continents;
-    vector<Territory*> territories;
-    string line;
-    string continentName;
-    Territory *aTerritory;
-
-    if (!mapFile) {
-        cerr << "Unable to open file";
-        exit(1);
-    }
-
-    while(getline(mapFile, line)) {
-        if(line == "[Continents]"){
-            while(getline(mapFile, line)) {
-                if(line == "[Territories]" || line.empty()) {
-                    break;
-                }
-                continentName = line.substr(0, line.size() - 2);
-                continents.push_back(continentName);
-            }
-        }
-        if(line == "[Territories]") {
-            while(getline(mapFile, line)) {
-                if(line.empty()) {
-                    continue;
-                }
-                aTerritory = new Territory(line.substr(0, line.find(',')));
-                territories.push_back(aTerritory);
-            }
-        }
-    }
-
-    mapFile.close();
-    cout << "Map Continents:" << endl;
-    for(const string& continent : continents) {
-        cout << continent << endl;
-    }
-
+//    cout << "Map Continents:" << endl;
+//    for(const string& continent : continents) {
+//        cout << continent << endl;
+//    }
+//
     cout << "\nMap Territories:" << endl;
     for(Territory *territory : territories) {
         cout << territory->getTerritoryName() << endl;
     }
-    delete aTerritory;
+//
+//    cout << "\nMap Adjacencies:" << endl;
+//    for(const string& adjacency : adjacencies) {
+//        cout << adjacency << endl;
+//    }
 }
 
 int main() {
     testLoadMaps();
     return 0;
 }
-
-
-
--- sources/Map/Map.h
+++ sources/Map/Map.h
@@ -1,47 +1,91 @@
 #ifndef MAP_H
 #define MAP_H
 
-#include <string>
+#pragma once
 #include "../Player/Player.h"
 
 class Player;
 
+class Continent {
+public:
+    Continent();
+    Continent(int map_continent_id, string continent_name);
+    ~Continent();
+    Continent(const Continent &continent);
+    Continent &operator = (const Continent &continent);
+    bool operator == (const Continent &continent) const;
+    friend ostream &operator << (ostream &outs, const Continent &continent);
+    int getMapContinentId();
+    string getContinentName();
+    vector<Territory*> getTerritories();
+    void addTerritory(Territory *territory);
+
+private:
+    int map_continent_id;
+    string continent_name;
+    vector<Territory*> territories;
+};
+
 class Territory {
 public:
-    Territory(Player *player, int numberOfArmies);
     Territory();
+    Territory(int map_territory_id, string territory_name, Continent *continent);
     ~Territory();
     Territory(const Territory &territory);
-    explicit Territory(const string &territoryName);
+    Territory &operator = (const Territory &territory);
+    bool operator == (const Territory &territory) const;
+    friend ostream &operator << (ostream &outs, const Territory &territory);
+    int getMapTerritoryId();
+    int getNumberOfArmies();
+    void setNumberOfArmies(int number_of_armies);
     string getTerritoryName();
+    vector<Territory*> getAdjacentTerritories();
+    Continent* getContinent();
+    Player* getTerritoryOwner();
+    void setTerritoryOwner(Player *player);
     string getPlayerName();
 
 private:
-    string territoryName;
+    int map_territory_id, number_of_armies;
+    string territory_name;
+    vector<Territory*> adjacent_territories;
+    Continent* continent;
     Player* player;
 };
 
 class Map {
 public:
-    explicit Map(Territory *territory);
     Map();
+    Map(vector<Territory*> territories, vector<Continent*> continents);
     ~Map();
     Map(const Map &map);
+    Map &operator = (const Map &map);
+    friend ostream &operator << (ostream &outs, const Map &map);
+    vector<Territory*> getTerritories();
+    vector<Continent*> getContinents();
+    Territory* getTerritory(int map_territory_id);
+    Territory* getTerritoryInContinent(int map_territory_id, int map_continent_id);
+    Continent* getContinent(int map_continent_id);
+    void depthFirstSearch(int starting_territory_id, vector<Territory*> &visited_territories);
+    void depthFirstSearch(vector<Territory*> continent, int starting_territory_id, vector<Territory*> &visited_territories);
     bool validate();
-    string getContinentName();
+
 
 private:
-    string continentName;
     vector<Territory*> territories;
+    vector<Continent*> continents;
 };
 
 class MapLoader {
 public:
     MapLoader();
     ~MapLoader();
-    MapLoader(const MapLoader &mapLoader);
+    MapLoader(const MapLoader &map_loader);
+    Map* loadMap(string map_file_directory);
 
 private:
+    string map_file_directory;
+    Map* map;
 };
 
 #endif //MAP_H
