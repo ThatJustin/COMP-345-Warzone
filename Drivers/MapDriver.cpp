f48ebae8b3701f600cb9d7a666c055a505d766ac
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2022-10-08 03:42:39 -0400

    added comments/doc-strings plus final map files testing with the addition of new test map files

--- Drivers/MapDriver.cpp
+++ Drivers/MapDriver.cpp
@@ -2,27 +2,46 @@
 
 using std::cout;
 using std::endl;
+using std::vector;
+using std::string;
 
+/**
+ * This function tests the loadMaps() and validate() functions/methods in the MapLoader and Map classes, respectively.
+ */
 void testLoadMaps() {
     auto *mapLoader = new MapLoader();
     vector<Map*> maps;
     vector<string> map_file_names;
     auto path = filesystem::path("./Map Files");
 
+    // Here, I am checking if the folder path does not exist
+    if(!exists(path)) {
+        cout << "The " << path.string().substr(2) << " folder does not exist" << endl;
+        return;
+    }
+
     cout << "Map file names:" << endl;
+    // Here, I am looping through all the map files in the Map Files folder and loading them one by one
     for(auto &map_file : filesystem::directory_iterator(path)) {
         if(map_file.path().string().substr(12) == ".DS_Store") {
             continue;
         }
         cout << "\tMap file: " << map_file.path().string().substr(12) << endl;
         map_file_names.push_back(map_file.path().string().substr(12));
+
+        // If the map file is not loadable, then it should be returning a null Map object, hence the check here
         if(mapLoader->loadMap(map_file.path().string()) == nullptr) {
+            cout << "\tThe map " << map_file.path().string().substr(12) << " cannot be loaded." << endl;
             map_file_names.pop_back();
             continue;
         }
         maps.push_back(mapLoader->loadMap(map_file.path().string()));
     }
 
+    // Here, I am making sure that I can't load a map file that does not even exist (this should return a null Map object)
+    mapLoader->loadMap("./Map Files/Map_File_That_Does_Not_Exist.map");
+
+    // Here, I am looping through all the generated/loaded maps from the Map Files folder and validating them one by one
     cout << "\nValidation of map files:" << endl;
     for(int i = 0; i < maps.size(); i++) {
         if(maps[i]->validate()) {
@@ -31,5 +50,6 @@ void testLoadMaps() {
             cout << "\t" << map_file_names[i] << " is an invalid map file" << endl;
         }
     }
+    // I make sure to delete the MapLoader object here before the program/process is finished, which deletes the Map objects that were loaded as well
     delete mapLoader;
 }
--- sources/Map/Map.h
+++ sources/Map/Map.h
@@ -1,89 +1,194 @@
+// Make sure to include the Map header only once when compiling
 #pragma once
-#include "../Player/Player.h"
 
+// Declare the classes used by the Map header before compiling it
 class Player;
+class Territory;
 
 class Continent {
 public:
+    // Default Constructor
     Continent();
+
+    // Parameterized Constructor
     Continent(int map_continent_id, const string &continent_name);
-    ~Continent();
+
+    // Copy Constructor
     Continent(const Continent &continent);
+
+    // Destructor
+    ~Continent();
+
+    // Assignment operator overload
     Continent &operator = (const Continent &continent);
-    bool operator == (const Continent &continent) const;
+
+    // Stream insertion operator overload
     friend ostream &operator << (ostream &outs, const Continent &continent);
-    int getMapContinentId();
+
+    // Accessor for the continent name
     string getContinentName();
+
+    // Accessor for the vector of territory pointers that point to territories found on the continent
     vector<Territory*> getTerritories();
+
+    // Adds a territory to the continent
     void addTerritory(Territory *territory);
 
 private:
+    // An integer identifier representing the continent id
     int map_continent_id;
+
+    // A string representing the continent name
     string continent_name;
+
+    // Collection/list of territory pointers that point to territories found on the continent
     vector<Territory*> territories;
 };
 
 class Territory {
 public:
+    // Default Constructor
     Territory();
-    explicit Territory(int map_territory_id, const string& territory_name, Player* player);
+
+    // Parameterized constructor taking a player as one of its parameters
+    Territory(int map_territory_id, const string& territory_name, Player *player);
+
+    // Parameterized constructor taking a continent as one of its parameters
     Territory(int map_territory_id, string territory_name, Continent *continent);
-    ~Territory();
+
+    // Copy Constructor
     Territory(const Territory &territory);
+
+    // Destructor
+    ~Territory();
+
+    // Assignment operator overload
     Territory &operator = (const Territory &territory);
-    bool operator == (const Territory &territory) const;
+
+    // Stream insertion operator overload
     friend ostream &operator << (ostream &outs, const Territory &territory);
+
+    // Accessor for the territory id
     int getMapTerritoryId();
+
+    // Mutator for the territory id, mainly used for the adjacent territories
     void setMapTerritoryId(int map_territory_id);
+
+    // Accessor for the number of armies on the territory
     int getNumberOfArmies();
+
+    // Mutator for the number of armies on the territory
     void setNumberOfArmies(int number_of_armies);
+
+    // Accessor for the territory name
     string getTerritoryName();
+
+    // Accessor for the vector of territory pointers that point to the adjacent territories of any specific territory object on the map
     vector<Territory*> getAdjacentTerritories();
+
+    // Accessor for the pointer to the continent the territory belongs to
     Continent* getContinent();
+
+    // Accessor for the pointer to the player that owns the territory
     Player* getTerritoryOwner();
+
+    // Mutator for the player that owns/will own the territory
     void setTerritoryOwner(Player *player);
+
+    // Accessor for the name of the player that owns the territory
     string getPlayerName();
-    void addAdjacentTerritory(Territory* territory);
+
+    // Adds an adjacent territory to the specified territory by passing the pointer to the adjacent territory as a parameter
+    void addAdjacentTerritory(Territory *territory);
 
 private:
+    // An integer identifier representing the territory id and the number of armies on the territory
     int map_territory_id, number_of_armies;
+
+    // A string representing the territory name
     string territory_name;
+
+    // Collection/list of territory pointers that point to adjacent territories of each specific territory object on the map
     vector<Territory*> adjacent_territories;
+
+    // A pointer to the continent the territory belongs to
     Continent* continent;
+
+    // A pointer to the player that owns the territory
     Player* player;
 };
 
 class Map {
 public:
+    // Default Constructor
     Map();
+
+    // Parameterized Constructor
     Map(vector<Territory*> territories, vector<Continent*> continents);
-    ~Map();
+
+    // Copy Constructor
     Map(const Map &map);
+
+    // Destructor
+    ~Map();
+
+    // Assignment operator overload
     Map &operator = (const Map &map);
+
+    // Stream insertion operator overload
     friend ostream &operator << (ostream &outs, const Map &map);
+
+    // Accessor for the vector of territory pointers that point to all the territories found on the map
     vector<Territory*> getTerritories();
+
+    // Accessor for the vector of continent pointers that point to all the continents found on the map
     vector<Continent*> getContinents();
+
+    // Accessor for the pointer to a specific territory given its respective territory id as a parameter
     Territory* getTerritory(int map_territory_id);
-    Territory* getTerritoryInContinent(int map_territory_id, int map_continent_id);
-    Continent* getContinent(int map_continent_id);
+
+    // DFS method used for traversing the map/graph in order to know if it is connected or not
     void depthFirstSearch(int starting_territory_id, vector<Territory*> &visited_territories);
-    void depthFirstSearch(Continent* continent, int starting_territory_id, vector<Territory*> &visited_territories);
+
+    // DFS method used for traversing the continents/subgraph in order to know if they each are connected or not
+    void depthFirstSearch(Continent *continent, int starting_territory_id, vector<Territory*> &visited_territories);
+
+    // Method used for validating the maps after they are loaded from the specific map files
     bool validate();
 
 
 private:
+    // Collection/list of territory pointers that point to all territories found on the map
     vector<Territory*> territories;
+
+    // Collection/list of continent pointers that point to all continents found on the map
     vector<Continent*> continents;
 };
 
 class MapLoader {
 public:
+    // Default Constructor
     MapLoader();
-    ~MapLoader();
+
+    // Copy Constructor
     MapLoader(const MapLoader &map_loader);
+
+    // Destructor
+    ~MapLoader();
+
+    // Assignment operator overload
+    MapLoader &operator = (const MapLoader &map_loader);
+
+    // Stream insertion operator overload
+    friend ostream &operator << (ostream &outs, const MapLoader &map_loader);
+
+    // Method used for loading the map from specific map files
     Map* loadMap(string map_file_directory);
 
 private:
+    // A string representing the specific map file directory to be loaded
     string map_file_directory;
+
+    // A pointer to the map object that is to be returned after the specific map file is loaded
     Map* map;
 };
