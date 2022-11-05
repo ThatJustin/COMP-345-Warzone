25e3df923be9b41bb321644c220d0461ff04b420
ThatJustin <62494960+ThatJustin@users.noreply.github.com>
2022-11-05 16:24:52 -0400

    feat: implemented testCommandProcessor

--- Drivers/CommandProcessingDriver.cpp
+++ Drivers/CommandProcessingDriver.cpp
@@ -10,14 +10,14 @@ using namespace std::literals;
 
 void testCommandProcessor(int argc, char** argv) {
     if (argc < 2 || (argv[1] != "-console"sv && argv[1] != "-file"sv)) {
-        cout<< "Please run the program using either -console or -file filename arguments." << endl;
+        cout << "Please run the program using either -console or -file filename arguments." << endl;
         exit(0);
     }
     bool isUsingConsole = true;
     string mapFileName;
     if (argv[1] == "-file"sv) {
         if (argc < 3) {
-            cout<< "Please include a file name for this argument like -file filename." << endl;
+            cout << "Please include a file name for this argument like -file filename." << endl;
             exit(0);
         }
         mapFileName = argv[2];
@@ -26,15 +26,38 @@ void testCommandProcessor(int argc, char** argv) {
 
     CommandProcessor* commandProcessor;
     if (isUsingConsole) {
-        commandProcessor  = new CommandProcessor(isUsingConsole, mapFileName);
+        commandProcessor = new CommandProcessor(isUsingConsole, mapFileName);
     } else {
         commandProcessor = new FileCommandProcessorAdapter(mapFileName);
     }
-    //TODO for testing, I will emulate the actual code here and not call the GameEngine
     GameEngine* gameEngine = new GameEngine();
     gameEngine->setCommandProcessor(commandProcessor);
+    gameEngine->changeStateByTransition(GameEngine::StartGame);
 
-    gameEngine->startupPhase();
-
-    delete commandProcessor;
+    while (true) {
+        string curStateName = gameEngine->getCurrentGameState()->name;
+        Command* c = commandProcessor->getCommand(curStateName);
+        if (c == nullptr) {
+            cout << "This command is not valid for this state." << endl;
+            continue;
+        }
+        if (c->getTransitionName() == "loadmap") {
+            gameEngine->commandParam = c->getParam();
+            gameEngine->changeStateByTransition(GameEngine::LoadMap);
+        } else if (c->getTransitionName() == "validatemap") {
+            gameEngine->changeStateByTransition(GameEngine::ValidateMap);
+        } else if (c->getTransitionName() == "addplayer") {
+            gameEngine->commandParam = c->getParam();
+            gameEngine->changeStateByTransition(GameEngine::AddPlayer);
+        } else if (c->getTransitionName() == "gamestart") {
+            cout << "Skipping main game loop for demo. Simulating a win transition.\r\n" << endl;
+            gameEngine->changeStateByTransition(GameEngine::Win);
+        } else if (c->getTransitionName() == "replay") {
+            gameEngine->prepareForReplay();
+            gameEngine->changeStateByTransition(GameEngine::StartGame);
+        } else if (c->getTransitionName() == "quit") {
+            cout << "Thanks for playing!" << endl;
+            exit(0);
+        }
+    }
 }
